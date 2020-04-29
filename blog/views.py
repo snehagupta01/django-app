c4f1937f35ebfe556dc7ae8c6e52e44b5c627a21
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-29 13:31:38 +0530

    +dummy data to home page

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+posts = [
+    {
+        'author': 'Sneha Gupta',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 29, 2020'
+    },
+    {
+        'author': 'Sg',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 29, 2020'
+    }
+]
 
 def home(request):
-    return render(request,'blog/home.html')
+    context = {
+        "posts":posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
