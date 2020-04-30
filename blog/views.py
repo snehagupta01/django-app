2a35ea3453158991324ac6df4635211898ce1351
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-30 18:51:50 +0530

    +creating post fn

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import ListView,DetailView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
 # Create your views here.
 from .models import Post
 
@@ -34,6 +40,14 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        form.instance.author=self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request,'blog/about.html',{'title':'ABOUT'})
