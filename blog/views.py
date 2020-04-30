0fa45dba868df977aa52a387e525104ec4485d2a
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-30 17:59:52 +0530

    showing post details

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView,DetailView
 # Create your views here.
 from .models import Post
 
@@ -23,6 +24,16 @@ def home(request):
         "posts":Post.objects.all()
     }
     return render(request,'blog/home.html',context)
+
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name='posts'
+    ordering=['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 
 def about(request):
     return render(request,'blog/about.html',{'title':'ABOUT'})
