937e68cf6566a61ad7184d9e44ad2fdd95e2df36
snehagupta01 <sneha2001gupta@gmail.com>
2020-04-30 10:46:01 +0530

    navbar fixed for login & logout

--- django_project/urls.py
+++ django_project/urls.py
@@ -20,8 +20,8 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
     path('login/',auth_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/',auth_views.LoginView.as_view(template_name='users/logout.html'),name='login'),
-    path('register/',user_views.register,name='register')
+    path('logout/',auth_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('register/',user_views.register,name='register'),
+    path('', include('blog.urls')),
 ]
